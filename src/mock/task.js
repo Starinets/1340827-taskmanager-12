ba2b3ad7c678911b4235af4f708bb7a81f8c8393
Starinets <tsn.ipad@gmail.com>
2020-08-04 04:22:11 +0600

    generate random data

--- src/mock/task.js
+++ src/mock/task.js
@@ -4,6 +4,8 @@ const DESCRIPTION = [
   `Пройти интенсив на соточку`
 ];
 
+const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 const getRandomInteger = (min = 0, max = 1) => {
@@ -19,11 +21,56 @@ const generateDescription = () => {
   return DESCRIPTION[randomIndex];
 };
 
-const generateTask = () => {
+const generateDate = () => {
+  // Когда в руках молоток, любая проблема - гвоздь.
+  // Вот и для генерации случайного булевого значения
+  // можно использовать "функцию из интернета".
+  // Ноль - ложь, один - истина. Для верности приводим
+  // к булевому типу с помощью Boolean
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+
+  // По заданию дедлайн у задачи устанавливается без учёта времеми,
+  // но объект даты без времени завести нельзя,
+  // поэтому будем считать срок у всех задач -
+  // это 23:59:59 установленной даты
+  currentDate.setHours(23, 59, 59, 999);
+
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
+const generateRepeating = () => {
   return {
-    description: generateDescription(),
-    dueDate: null,
-    repeating: {
+    mo: false,
+    tu: false,
+    we: Boolean(getRandomInteger(0, 1)),
+    th: false,
+    fr: Boolean(getRandomInteger(0, 1)),
+    sa: false,
+    su: false
+  };
+};
+
+const getRandomColor = () => {
+  const randomIndex = getRandomInteger(0, COLORS.length - 1);
+
+  return COLORS[randomIndex];
+};
+
+const generateTask = () => {
+  const dueDate = generateDate();
+  const repeating = dueDate === null
+    ? generateRepeating()
+    : {
       mo: false,
       tu: false,
       we: false,
@@ -31,10 +78,15 @@ const generateTask = () => {
       fr: false,
       sa: false,
       su: false
-    },
-    color: `black`,
-    isArchive: false,
-    isFavorite: false
+    };
+
+  return {
+    description: generateDescription(),
+    dueDate,
+    repeating,
+    color: getRandomColor(),
+    isArchive: Boolean(getRandomInteger(0, 1)),
+    isFavorite: Boolean(getRandomInteger(0, 1))
   };
 };
 
