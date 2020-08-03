8e46671e89f09e5582e89a291bddc934e20d9ab3
Starinets <tsn.ipad@gmail.com>
2020-08-04 03:39:20 +0600

    configure webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const PATH_TO_BUNDLE = path.join(__dirname, 'public');
 
 module.exports = {
   mode: 'development',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public'),
+    path: PATH_TO_BUNDLE,
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: PATH_TO_BUNDLE,
+    watchContentBase: true,
+  }
 };
