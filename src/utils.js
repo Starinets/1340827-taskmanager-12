a4651a38b1287fc2c01c28f8e0736d3f132a4f98
Starinets <tsn.ipad@gmail.com>
2020-08-06 14:15:17 +0600

    filters

--- src/utils.js
+++ src/utils.js
@@ -1,35 +1,43 @@
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-const getRandomInteger = (min = 0, max = 1) => {
+export const getRandomInteger = (min = 0, max = 1) => {
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-const isTaskExpired = (dueDate) => {
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+
+  return new Date(currentDate);
+};
+
+export const isTaskExpired = (dueDate) => {
   if (dueDate === null) {
     return false;
   }
 
-  let currentDate = new Date();
-  currentDate.setHours(23, 59, 59, 999);
-  currentDate = new Date(currentDate);
+  const currentDate = getCurrentDate();
 
   return currentDate.getTime() > dueDate.getTime();
 };
 
-const isTaskRepeating = (repeating) => {
+export const isTaskExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() === dueDate.getTime();
+};
+
+export const isTaskRepeating = (repeating) => {
   return Object.values(repeating).some(Boolean);
 };
 
-const humanizeTaskDueDate = (dueDate) => {
+export const humanizeTaskDueDate = (dueDate) => {
   return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
-};
-
-export {
-  getRandomInteger,
-  isTaskExpired,
-  isTaskRepeating,
-  humanizeTaskDueDate
 };
--- src/view/task.js
+++ src/view/task.js
@@ -1,9 +1,9 @@
 import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from "../utils.js";
 
-const createTaskTemplate = (task) => {
+export const createTaskTemplate = (task) => {
   const {color, description, dueDate, repeating, isArchive, isFavorite} = task;
 
-  const dateLocaleString = dueDate !== null
+  const date = dueDate !== null
     ? humanizeTaskDueDate(dueDate)
     : ``;
 
@@ -16,57 +16,53 @@ const createTaskTemplate = (task) => {
     : ``;
 
   const archiveClassName = isArchive
-    ? `card__btn--disabled`
-    : ``;
+    ? `card__btn--archive card__btn--disabled`
+    : `card__btn--archive`;
 
   const favoriteClassName = isFavorite
-    ? `card__btn--disabled`
-    : ``;
+    ? `card__btn--favorites card__btn--disabled`
+    : `card__btn--favorites`;
 
-  return (
-    `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
-      <div class="card__form">
-        <div class="card__inner">
-          <div class="card__control">
-            <button type="button" class="card__btn card__btn--edit">
-              edit
-            </button>
-            <button type="button" class="card__btn card__btn--archive ${archiveClassName}">
-              archive
-            </button>
-            <button
-              type="button"
-              class="card__btn card__btn--favorites ${favoriteClassName}"
-            >
-              favorites
-            </button>
-          </div>
+  return `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
+    <div class="card__form">
+      <div class="card__inner">
+        <div class="card__control">
+          <button type="button" class="card__btn card__btn--edit">
+            edit
+          </button>
+          <button type="button" class="card__btn ${archiveClassName}">
+            archive
+          </button>
+          <button
+            type="button"
+            class="card__btn ${favoriteClassName}"
+          >
+            favorites
+          </button>
+        </div>
 
-          <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
+        <div class="card__color-bar">
+          <svg class="card__color-bar-wave" width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+        </div>
 
-          <div class="card__textarea-wrap">
-            <p class="card__text">${description}</p>
-          </div>
+        <div class="card__textarea-wrap">
+          <p class="card__text">${description}</p>
+        </div>
 
-          <div class="card__settings">
-            <div class="card__details">
-              <div class="card__dates">
-                <div class="card__date-deadline">
-                  <p class="card__input-deadline-wrap">
-                    <span class="card__date">${dateLocaleString}</span>
-                  </p>
-                </div>
+        <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              <div class="card__date-deadline">
+                <p class="card__input-deadline-wrap">
+                  <span class="card__date">${date}</span>
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </article>`
-  );
+    </div>
+  </article>`;
 };
-
-export {createTaskTemplate};
